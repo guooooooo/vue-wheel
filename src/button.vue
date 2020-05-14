c8cd8f21fadb15c17972a7d391ebf4617d1de681
guolinhao <gavin141113@163.com>
2020-05-14 11:41:36 +0800

    引入parcel 实现基本button

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="w-button"><slot></slot></button>
+</template>
+<script>
+  export default {
+    name: 'wheel-button'
+  }
+</script>
+<style lang="scss">
+  .w-button {
+    height: var(--button-height);
+    padding: 0 1em;
+    font: inherit;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
